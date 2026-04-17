89dbf2066ff3ed7d138448ca06d3c20cdbf85d28
Joshbbdroid5 <Jajjabee430iyu@gmail.com>
2026-04-17 10:46:25 -0700

    fix: update dashboard import case sensitivity

--- client/App.tsx
+++ client/App.tsx
@@ -8,7 +8,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Dashboard from "./pages/dashboard";
+import Dashboard from "./pages/Dashboard";
 import BoardSelection from "./pages/BoardSelection";
 import Game from "./pages/Game";
 
